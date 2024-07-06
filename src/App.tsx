6f536d2093ce2002715f03f35e79bb5468523080
rczerwiec <czerwiecr1999@gmail.com>
2024-07-06 13:17:15 +0200

    Website layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,18 @@ import React from 'react';
 
 function App() {
   return (
-    <div className='flex'>
+    <div className='flex h-screen w-screen justify-center items-center'>
+      <div className='absolute'>LOGO</div>
+      <div className='absolute right-0 bottom-0'>
+        <div className='flex'>
+          <div>ICON</div>
+          <div>kontakt@kontakt.pl</div>
+        </div>
+        <div className='flex'>
+          <div>ICON</div>
+          <div>+48 000 000 000</div>
+        </div>
+      </div>
 
     </div>
 
