880a37b8eac1c23a53989ac9331dd5cf8714e3c8
rczerwiec <czerwiecr1999@gmail.com>
2024-07-06 14:39:17 +0200

    responsive for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className='flex h-screen w-screen justify-center items-center'>
       <div className='absolute'>LOGO</div>
-      <div className='absolute right-0 bottom-0'>
+      <div className='absolute bottom-0 md:right-0 md:bottom-0'>
         <div className='flex'>
           <div>ICON</div>
           <div>kontakt@kontakt.pl</div>
