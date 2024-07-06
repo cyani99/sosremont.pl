b85a5c07f0a07e6f0c1d4466a58dbc14dd9ca01f
rczerwiec <czerwiecr1999@gmail.com>
2024-07-06 12:18:47 +0200

    project refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import logo from './sosremont.png';
-import './App.css';
 
 function App() {
   return (
-    <div className='w-screen animated-background h-screen bg-gradient-to-r from-stone-950 via-stone-900 to-zinc-900'>
-      <p className="flex h-screen w-full justify-center items-center">
-            <img src={logo} className="w-100" alt="logo" />
-      </p>
+    <div className='flex'>
+
     </div>
 
   );
