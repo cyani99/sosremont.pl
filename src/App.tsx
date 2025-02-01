6a8e59c179818d11eef3cb8a1ddf1b9982e6b9be
cyani99 <czerwiecr1999@icloud.com>
2025-02-01 18:04:25 +0100

    CHANGE: little ui redesign

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,22 @@ import { FaPhone } from "react-icons/fa6";
 
 function App() {
   return (
-    <div className='flex h-screen w-screen justify-center items-center'>
-        <img className='absolute w-[100%] md:w-[30%]' alt="logo" src={logo}></img>
-      <div className='absolute bottom-0 md:right-0 md:bottom-0
-                      text-white font-teko text-2xl
-                      p-6'>
-        <div className='flex'>
-          <MdEmail className='mr-2 text-custom-gold'/>
-          <div>kontakt@kontakt.pl</div>
+    <div className='flex h-screen w-screen justify-center items-center bg-[#1A1A1A] relative'>
+      {/* Logo */}
+      <img className='absolute w-[80%] md:w-[30%] max-w-sm' alt="logo" src={logo} />
+      
+      {/* Informacje kontaktowe */}
+      <div className='absolute bottom-6 md:right-6 text-white font-teko text-2xl p-4 bg-opacity-50 bg-black rounded-lg shadow-lg'>
+        <div className='flex items-center mb-2'>
+          <MdEmail className='mr-2 text-custom-gold text-3xl' />
+          <div className='text-lg'>kontakt@sosremont.pl</div>
         </div>
-        <div className='flex '>
-          <FaPhone className='mr-2 text-custom-gold' />
-          <div>+48 000 000 000</div>
+        <div className='flex items-center'>
+          <FaPhone className='mr-2 text-custom-gold text-3xl' />
+          <div className='text-lg'>+48 579 644 075</div>
         </div>
       </div>
-
     </div>
-
   );
 }
 
